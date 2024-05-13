cfeba23b65e99d92e98706d863fdf4a7caee0859
Albert Peto <petoalbert32@gmail.com>
2024-05-13 20:11:49 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ImageTransform/', // Specify your base path here
 })
